5f950f21e65460ffc088f1636d4984d751608483
Patel Manthan <patelmanthan1806@gmail.com>
2021-02-26 15:00:12 +0530

    promise

---
+++ promise-api.js
@@ -0,0 +1,22 @@
+const p1 = new Promise((resolve,) => {
+    setTimeout(() => {
+        console.log('Asynchronous operation...');
+        resolve(1);
+    }, 2000);
+});
+
+const p2 = new Promise((resolve,) => {
+    setTimeout(() => {
+        console.log('Asynchronous operation...');
+        resolve(2);
+    }, 2000);
+});
+
+/* Promise.all([p1, p2])
+    .then(result => console.log(result))
+    .catch(err => console.log('Error', err.message)); */
+
+
+Promise.race([p1, p2])
+    .then(result => console.log(result))
+    .catch(err => console.log('Error', err.message));
--- promise.js
+++ promise.js
@@ -8,3 +8,22 @@ const p = new Promise((resolve, reject) => {
 p
     .then(result => console.log('Result', result))
     .catch(err => console.log('Error', err.message))
+
+    /* console.log('Before');
+
+    getUser (1, (user) => {
+        getRepositories(user.gitHubUsername, (repos) => {
+            getCommits(repos[0], (commits) => {
+                console.log(commits)
+            })
+        })
+    }); */
+
+    /* getUser(1)
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits', commits))
+    .catch(err => console.log('Error', err.message));
+
+    console.log('after');
+ */
